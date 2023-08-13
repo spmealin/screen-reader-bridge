202f53801f5e61b46fc964f8fddbfd3e82429a31
Sean Mealin <spmealin@gmail.com>
2023-08-13 19:44:15 -0400

    Adds build system and ScreenReaderBridge class

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import del from "rollup-plugin-delete";
+
+// Modified from: https://gist.github.com/rikkit/b636076740dfaa864ce9ee8ae389b81c#file-tsconfig-json
+
+export default [
+    {
+        input: "src/ScreenReaderBridge.ts",
+        output: [
+            {
+                file: "dist/index.js",
+                name: "ScreenReaderBridge",
+                format: "iife"
+            }
+        ],
+        plugins: [typescript({ tsconfig: "./tsconfig.json" })]
+    },
+    {
+        input: "src/ScreenReaderBridge.ts",
+        output: [
+            {
+                file: "dist/index.mjs",
+                format: "es"
+            }
+        ],
+        plugins: [typescript({ tsconfig: "./tsconfig.json" })]
+    },
+    {
+        input: "dist/types/ScreenReaderBridge.d.ts",
+        output: [
+            {
+                file: "dist/index.d.ts",
+                format: "es",
+                plugins: []
+            }
+        ],
+        plugins: [dts(), del({ targets: "dist/types", hook: "buildEnd" })]
+    }
+];
